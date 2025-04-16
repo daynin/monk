c7918f28f7a64c709ffefff7db12491a9bfb4aed
Sergey Golovin <sergey.golovin@protonmail.com>
2025-04-16 17:31:26 -0300

    chore: add flake.nix

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "Monk - A simple Git hooks manager in written Rust";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ rust-overlay.overlays.default ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+
+        rustToolchain = pkgs.rust-bin.stable.latest.default;
+
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            rustToolchain
+            pkg-config
+          ];
+
+          RUST_SRC_PATH = "${rustToolchain}/lib/rustlib/src/rust/library";
+        };
+
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "monk";
+          version = "0.2.0";
+          src = ./.;
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+          };
+          nativeBuildInputs = with pkgs; [ pkg-config ];
+        };
+      });
+}
